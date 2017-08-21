6744097db067f1ec339a869dea4fa04b30787de3
caojin <caojin@192.168.62.23>
2017-08-21 09:34:14 +0800

    文件修改

---
+++ src/main/java/com/k12ct/demo/UserDomain.java
@@ -0,0 +1,26 @@
+package com.k12ct.demo;
+
+public class UserDomain {
+	  private int id;
+	  private String name;
+	  private String sec;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSec() {
+		return sec;
+	}
+	public void setSec(String sec) {
+		this.sec = sec;
+	}
+	  
+}

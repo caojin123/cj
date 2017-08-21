1088889d1140eb5a27b69b651f8bca804924dcd8
caojin <caojin@192.168.62.23>
2017-08-21 09:30:17 +0800

    修改文件

--- src/main/java/com/k12ct/demo/UserController.java
+++ src/main/java/com/k12ct/demo/UserController.java
@@ -5,44 +5,47 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dianping.cat.Cat;
 import com.dianping.cat.message.Event;
-import com.dianping.cat.message.Transaction;  
+import com.dianping.cat.message.Transaction;
+
 
 @Controller
-@RequestMapping("/hello")
-public class HelloController {
- 
-    @RequestMapping("/hello")
-    public String hello(){    
+@RequestMapping("/user")
+public class UserController {
+
+	
+	
+    @RequestMapping("/query")
+    public String query(){    
     	
     	String pageName = "helloworld";
-    	String serverIp = "192.168.1.222";
+    	String serverIp = "127.0.0.1";
     	double amount = 0;
+    /**
+     *  用transaction记录一段代码的执行时间，这里的时间是指从创建至complete之间的耗时。
+     *  Event记录一个事件，如：一次远程调用的地址。
+     *  Metric记录一个业务指标。
+     */
     	
     	Transaction t = Cat.newTransaction("URL", pageName);
     	
     	try {
     		
+    		 //记录一个事件
 			Cat.logEvent("URL.Server", serverIp, Event.SUCCESS, "ip="+ serverIp + "&...");
-			
+			//记录一个业务指标，记录次数
 			Cat.logMetricForCount("PayCount");
-			
+			//
 			Cat.logMetricForSum("PayAmont", amount);
 			
-			t.setStatus(Transaction.SUCCESS);
+			t.setStatus(Transaction.SUCCESS);//设置状态
 			
-			//Thread.sleep(3000);
-//			String abc = null;
-//			abc.length();
-			
+			//业务代码
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Cat.getProducer().logError(e);
 			t.setStatus(e);
 		} finally {
 			t.complete();
 		}
-    	
-    	
         return "hello";
     }
 }
